c1e3ba53c61ab1a33499807286fb7ff9ab930e28
unknown <jv@localhost.com>
2023-03-08 19:42:58 +0100

    Refactor a clase

--- Backtraking/Program.cs
+++ Backtraking/Program.cs
@@ -1,96 +1,24 @@
 ﻿using System;
+using BackTracking;
 
-namespace BackTracking
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        // Tamaño del tablero
-        const int N = 8;
+        // Celdas del tablero NxN
+        // Movimientos posibles de la pieza en coordenadas (x,y) - valor inicial para coordenada 1..
+        int celdas = 8;
+        (int X, int Y)[] movPieza = { (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1) };
 
-        /// <summary>
-        /// El movimiento será válido si está dentro del tablero y la casilla == -1 --> no ha sido visitada
-        /// </summary>
-        /// <param name="celda">Celda que se comprobará si exite en el tablero y si no ha sido visitada aún</param>
-        /// <param name="tablero">Tablero donde se comprueba la celda pasada a la función</param>
-        /// <returns></returns>
-        // 
-        static bool movimientoValido((int x, int y) celda, int[,] tablero)
-        {
-            return (celda.x >= 0 && celda.x < N && celda.y >= 0 && celda.y < N && tablero[celda.x, celda.y] == -1);
-        }
+        // Inicializar tablero para tamaño dado, pieza concreta y coordenadas de la celda inicial
+        Tablero tablero = new Tablero(celdas, movPieza, 1, 1);
 
-        /// <summary>
-        /// Función recursiva - NOTA: podría implementarse usando Queue para evitar recursiones -
-        /// Dada una posición y teniendo en cuenta el número de movimiento actual (movimiento i-esimo)
-        /// se intenta mover el caballo a todas sus posibles posiciones (8 en total)
-        /// Si el movimiento no es válido se descarta
-        /// Si el movimiento es válido se marca la celda actual con el número de movimiento actual
-        /// y se repite el proceso usando la posicion nueva y el número de movimiento nuevo (movimiento i-esimo + 1)
-        /// Si el movimiento i-esimo es el N*N = total de celdas estamos en el último movimiento de la solución (TRUE)
-        /// Si el movimiento no es el último y no hay movimientos válidos desde la casilla actual, no hay solución (FALSE)
-        /// </summary>
-        /// <param name="celda">Tupla con las coordenadas X, Y de la celda actual</param>
-        /// <param name="mov_i">Movimiento i-esimo de la posible solución</param>
-        /// <param name="tableroSolucion">Matriz que contendrá la solución final</param>
-        /// <param name="movimientosPieza">Array de tuplas con todos los posibles movimientos de la pieza (desplazamiento en ejes x,y)</param>
-        /// <returns></returns>
-        static bool resolverTablero((int x, int y) celda, int mov_i, int[,] tableroSolucion, (int x, int y)[] movimientosPieza)
-        {
-            if (mov_i == N * N)
-                return true;
-
-            for (int m = 0; m < 8; m++)
-            {
-                (int x, int y) celdaSiguiente = (celda.x + movimientosPieza[m].x , celda.y + movimientosPieza[m].y);
-                if (movimientoValido(celdaSiguiente, tableroSolucion))
-                {
-                    tableroSolucion[celdaSiguiente.x, celdaSiguiente.y] = mov_i;
-                    if (resolverTablero(celdaSiguiente, mov_i + 1, tableroSolucion, movimientosPieza))
-                        return true;
-                    else
-                        tableroSolucion[celdaSiguiente.x, celdaSiguiente.y] = -1; // Backtracking
-                }
-            }
-            return false;
-        }
-
-        static void imprimirTablero(int[,] solucionTablero)
-        {
-            for (int x = 0; x < N; x++)
-            {
-                Console.WriteLine("+----+----+----+----+----+----+----+----+");
-                for (int y = 0; y < N; y++)
-                    Console.Write("| " + solucionTablero[x, y].ToString("D2") + " ");
-                Console.WriteLine("|");
-            }
-            Console.WriteLine("+----+----+----+----+----+----+----+----+");
-        }
-
-        static void Main(string[] args)
-        {
-            // Posibles movimientos del caballo
-            (int X, int Y)[] movPieza = { (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1) };
-            
-            // Matriz que guardará la solución con el número de salto en cada celda
-            int[,] solucionTablero = new int[N, N]; 
-
-            // Todas las celdas comienzan con -1 para marcarlas como no visitadas
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++)
-                    solucionTablero[i, j] = -1;
-
-            // Casilla de salida. Es la primera solución, el movimiento 0
-            (int X, int Y) celda = (0, 0);
-            solucionTablero[celda.X, celda.Y] = 0; 
-
-            Console.WriteLine("Recorrido de tablero de ajedrez usando un caballo:\n");
-            if (resolverTablero(celda, 1, solucionTablero, movPieza))
-                imprimirTablero(solucionTablero);
-            else
-                Console.WriteLine("No hay solución");
-            
-            Console.WriteLine("\nPulsa ENTER para salir...");
-            Console.ReadLine();
-        }
+        Console.WriteLine("Recorrido de tablero de ajedrez usando un caballo:\n");
+        if (tablero.resolverTablero())
+            tablero.imprimirTablero();
+        else
+            Console.WriteLine("No hay solución");
+        Console.WriteLine("\nPulsa ENTER para salir...");
+        Console.ReadLine();
     }
 }
---
+++ Backtraking/Tablero.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace BackTracking
+{
+    internal class Tablero
+    {
+        // Tamaño del tablero. No está pensado para que sea cambiado. Se trata de un tablero de ajedrez de 8x8
+        public int N; // = 8;
+        public int[,] tableroSolucion;
+        // Casilla de salida. Es la primera solución, el movimiento 0
+        public (int X, int Y) celda; // = (0, 0);
+        // Posibles movimientos del caballo
+        (int X, int Y)[] movPieza; // = { (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1) };
+        public bool solucionado; // = false;
+
+        /// <summary>
+        /// Tablero se inicia con celdas (NxN), movimientos de la pieza a usar, coordenada X y coordenada Y de la celda de inicio
+        /// </summary>
+        /// <param name="celdas">Dimensiones del tablero (NxN)</param>
+        /// <param name="movimientos">Posibles movimientos de la pieza</param>
+        /// <param name="x">Coordenada X de celda de origen</param>
+        /// <param name="y">Coordenada Y de celda de origen</param>
+        public Tablero (int celdas, (int X, int Y)[] movimientos , int x, int y)
+        {
+            N = celdas;
+            celda = (x-1, y-1); // -1 para poder comenzar en 1,1 en vez de en 0,0 al llamar al constructor
+            movPieza = movimientos;
+            tableroSolucion = new int[N, N];
+            solucionado = false;
+
+            iniciarTableroSoluciones();
+        }
+
+        /// <summary>
+        /// El movimiento será válido si está dentro del tablero y la casilla == -1 --> no ha sido visitada
+        /// </summary>
+        /// <param name="celda">Celda que se comprobará si exite en el tablero y si no ha sido visitada aún</param>
+        /// <param name="tablero">Tablero donde se comprueba la celda pasada a la función</param>
+        /// <returns>Indica si es el movimiento es vaído (true) o no (false)</returns> 
+        bool movimientoValido((int x, int y) celda, int[,] tablero)
+        {
+            return (celda.x >= 0 && celda.x < N && celda.y >= 0 && celda.y < N && tablero[celda.x, celda.y] == -1);
+        }
+
+        void iniciarTableroSoluciones ()
+        {
+            // Todas las celdas comienzan con -1 para marcarlas como no visitadas
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                    tableroSolucion[i, j] = -1;
+            tableroSolucion[celda.X, celda.Y] = 0; // Primera solución - punto de partida -
+        }
+
+        public bool resolverTablero()
+        {
+            iniciarTableroSoluciones();
+            solucionado = (resolverTablero_pri(celda, 1, tableroSolucion));
+            return solucionado;
+        }
+
+        /// <summary>
+        /// Función recursiva - NOTA: podría implementarse usando Queue para evitar recursiones -
+        /// Dada una posición y teniendo en cuenta el número de movimiento actual (movimiento i-esimo)
+        /// se intenta mover el caballo a todas sus posibles posiciones (8 en total)
+        /// Si el movimiento no es válido se descarta
+        /// Si el movimiento es válido se marca la celda actual con el número de movimiento actual
+        /// y se repite el proceso usando la posicion nueva y el número de movimiento nuevo (movimiento i-esimo + 1)
+        /// Si el movimiento i-esimo es el N*N = total de celdas estamos en el último movimiento de la solución (TRUE)
+        /// Si el movimiento no es el último y no hay movimientos válidos desde la casilla actual, no hay solución (FALSE)
+        /// </summary>
+        /// <param name="celda">Tupla con las coordenadas X, Y de la celda actual</param>
+        /// <param name="mov_i">Movimiento i-esimo de la posible solución</param>
+        /// <param name="tableroSolucion">Matriz que contendrá la solución final</param>
+        /// <returns>Devuelve si se resolvió (true) o no (false)</returns>
+        public bool resolverTablero_pri((int x, int y) celda, int mov_i, int[,] tableroSolucion)
+        {
+            if (mov_i == N * N)
+                return true;
+
+            for (int m = 0; m < movPieza.Length; m++)
+            {
+                (int x, int y) celdaSiguiente = (celda.x + movPieza[m].X, celda.y + movPieza[m].Y);
+                if (movimientoValido(celdaSiguiente, tableroSolucion))
+                {
+                    tableroSolucion[celdaSiguiente.x, celdaSiguiente.y] = mov_i;
+                    if (resolverTablero_pri(celdaSiguiente, mov_i + 1, tableroSolucion))
+                        return true;
+                    else
+                        tableroSolucion[celdaSiguiente.x, celdaSiguiente.y] = -1; // Backtracking
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Dado un tablero con pasos en cada celda, se devuelve un array de tuplas ordenadas de primer a último paso
+        /// </summary>
+        /// <param name="tableroSolucion">Tablero con pasos completados</param>
+        /// <returns>Array ordenado de las coordenadas de los pasos dados para solucionar el caso</returns>
+        public (int, int)[] recorridoSolucion(int[,] tableroSolucion)
+        {
+            (int x, int y)[] recorridoSolucion = new (int, int)[N * N];
+            for (int movimiento = 0; movimiento < N * N; movimiento++)
+                for (int y = 0; y < N; y++)
+                    for (int x = 0; x < N; x++)
+                        if (tableroSolucion[x, y] == movimiento)
+                            recorridoSolucion[movimiento] = (y, x);
+
+            return recorridoSolucion;
+        }
+
+        public void imprimirTablero()
+        {
+            if (!solucionado)
+                return;
+
+            string separador = "";
+            for (int NCeldas = 0; NCeldas < N; NCeldas++)
+                separador += "+----";
+            separador += "+";
+
+            for (int x = 0; x < N; x++)
+            {
+                Console.WriteLine(separador);
+                for (int y = 0; y < N; y++)
+                    Console.Write("| " + tableroSolucion[x, y].ToString("D2") + " ");
+                Console.WriteLine("|");
+            }
+            Console.WriteLine(separador);
+
+            Console.WriteLine("\nRecorrido realizado:\n");
+            int movimiento = 0;
+            foreach ((int x, int y) celda in recorridoSolucion(tableroSolucion))
+            {
+                if (++movimiento > N)
+                {
+                    Console.WriteLine();
+                    movimiento = 1;
+                }
+                Console.Write("(" + (1+celda.x).ToString() + ", " + (1+celda.y).ToString() + ")  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
