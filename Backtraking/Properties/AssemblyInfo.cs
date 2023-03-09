01398aacbee44e4e9f5353171a36d55ccd8c9ca9
Jose <122695051+JoseVIGIT@users.noreply.github.com>
2023-03-09 13:52:34 +0100

    Actualizar AssemblyInfo.cs

--- Backtraking/Properties/AssemblyInfo.cs
+++ Backtraking/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("MovCaballo")]
+[assembly: AssemblyTitle("Backtracking")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MovCaballo")]
+[assembly: AssemblyProduct("Backtracking")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
